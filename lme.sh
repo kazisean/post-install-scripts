572e1cc81e300f4796e10f4ee83d597cc43e2fac
kazisean <keh8423@nyu.edu>
2023-08-21 10:14:15 -0400

    save

---
+++ lme.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Lan computer setup and maintenance script (Lan Made Easy)
+# by Kazi Hossain <kazi.h@nyu.edu>
+# License: GNU GPLv3
+
+### check if the computer is running windows or mac ###
