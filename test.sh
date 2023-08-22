2bc55f54c0a64352916b78da142b0e6c21994161
kazisean <keh8423@nyu.edu>
2023-08-22 12:53:34 -0400

    aa

--- test.sh
+++ test.sh
@@ -5,11 +5,27 @@
 
 # Try running Homebrew shell environment setup
 eval "$(/opt/homebrew/bin/brew shellenv)"
-
-
 if [ $? -ne 0 ]; then
     # If the previous command failed, install Homebrew
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" > /dev/null 2>&1 &
+    
+    # Wait for the installation to complete
+    wait $!
+    
+    # Check the installation status
+    if [ $? -eq 0 ]; then
+        echo "Homebrew installation successful."
+        
+        # Now that Homebrew is installed, try setting up the environment again
+        eval "$(/opt/homebrew/bin/brew shellenv)"
+        if [ $? -ne 0 ]; then
+            echo "Error setting up Homebrew environment after installation."
+            exit 1
+        fi
+    else
+        echo "Homebrew installation failed."
+        exit 1
+    fi
     
     # Quit the terminal after installing Homebrew
     osascript -e 'tell application "Terminal" to quit'
