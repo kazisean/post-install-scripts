fd6173f1c6724956c0a4f4df84492c408972cb20
kazisean <keh8423@nyu.edu>
2023-08-22 12:36:54 -0400

    save

--- test.sh
+++ test.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-# Try to put homebrew in path
-installation_instructions='echo "export PATH=/usr/local/bin:$PATH" >> ~/.bash_profile && source ~/.bash_profile'
+# Try to put Homebrew in path
+step_1='echo; echo "eval \"\$(/opt/homebrew/bin/brew shellenv)\"" >> "/Users/$(whoami)/.zprofile"'
+step_2='eval "$(/opt/homebrew/bin/brew shellenv)"'
 
-# Run installation instructions and check for errors
-if ! eval "$installation_instructions"; then
-    echo "Error adding Homebrew to PATH. Downloading and installing Homebrew..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-    # Run the installation instructions again
-    eval "$installation_instructions"
-else
-    echo "Homebrew is already in PATH."
+# Run step 1 and check for errors
+if ! eval "$step_1"; then
+    echo "Adding Homebrew to PATH failed."
+    if ! eval "$step_2"; then
+        echo "Running step 2 failed. Downloading and installing Homebrew..."
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        eval "$step_2"
+    else
+        echo "Homebrew is already in PATH."
+    fi
 fi
 
 # Refresh the terminal
