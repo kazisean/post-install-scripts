42eb75aa0b84052ffc2447cfd7818150be3bbc88
kazisean <keh8423@nyu.edu>
2023-08-22 12:16:45 -0400

    mind game lol

--- test.sh
+++ test.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-# Download and install Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+homebrew_dir="/out/homebrew"
 
-# Find the installation instructions in the output gre the path command and run it
-installation_instructions=$(brew help 2>&1 | awk '/- Run these two commands in your terminal to add Homebrew to your PATH:/,/- Run brew help to get started/' | sed '1d;$d')
-
-# Run the installation instructions
-eval "$installation_instructions"
+# Check if the directory exists
+if [ ! -d "$homebrew_dir" ]; then
+    # Directory doesn't exist, run the Homebrew installation command
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    # Directory exists, proceed with captured installation instructions
+    installation_instructions=$(brew help 2>&1 | awk '/- Run these two commands in your terminal to add Homebrew to your PATH:/,/- Run brew help to get started/' | sed '1d;$d')
+    eval "$installation_instructions"
+fi
 
 # Refresh the terminal
 exec "$SHELL"
