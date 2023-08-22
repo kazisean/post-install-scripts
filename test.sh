1014088e75c4524d483208a2447a14de073f058b
kazisean <keh8423@nyu.edu>
2023-08-22 12:41:30 -0400

    fixed

--- test.sh
+++ test.sh
@@ -11,10 +11,13 @@ if ! eval "$step_1"; then
         echo "Running step 2 failed. Downloading and installing Homebrew..."
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
         eval "$step_2"
+
+        # Reload the modified .zprofile
+        source "/Users/$(whoami)/.zprofile"
     else
         echo "Homebrew is already in PATH."
     fi
 fi
 
-# Refresh the terminal
+# Reload the terminal
 exec "$SHELL"
