118d7dfdfa3ea6f61d292856c4154f51123de4d1
kazisean <keh8423@nyu.edu>
2023-08-22 12:31:15 -0400

    this should do it

--- test.sh
+++ test.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-homebrew_dir="/opt/homebrew"
+# Try to put homebrew in path
+installation_instructions='echo "export PATH=/usr/local/bin:$PATH" >> ~/.bash_profile && source ~/.bash_profile'
 
-# Attempt to change to the directory
-if cd "$homebrew_dir" 2>/dev/null; then
-    # Directory exists, proceed with captured installation instructions
-    installation_instructions=$(brew help 2>&1 | awk '/- Run these two commands in your terminal to add Homebrew to your PATH:/,/- Run brew help to get started/' | sed '1d;$d')
+# Run installation instructions and check for errors
+if ! eval "$installation_instructions"; then
+    echo "Error adding Homebrew to PATH. Downloading and installing Homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+    # Run the installation instructions again
     eval "$installation_instructions"
 else
-    # Directory doesn't exist, run the Homebrew installation command
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    echo "Homebrew is already in PATH."
 fi
 
 # Refresh the terminal
