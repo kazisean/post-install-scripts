5716f45bc8f6e15f05ea6c1813763fe63daffbad
Kazi Sean <keh8423@nyu.edu>
2023-08-30 18:21:07 -0700

    Update change-dns.ps1

--- windows/change-dns.ps1
+++ windows/change-dns.ps1
@@ -1,33 +1,8 @@
-# define your dns servers below in between the quotes. Running this script as it is will change your ethernet connection DNS to cloudflare dns 
-$dnsServers = "1.1.1.1", "1.0.0.1"
+# run this command to reset your dns server to default dns Set-DnsClientServerAddress -InterfaceAlias "Ethernet" -ResetServerAddresses
 
-# Get all Ethernet network adapters
-$ethernetAdapters = Get-NetAdapter | Where-Object { $_.InterfaceDescription -like "*Ethernet*" }
+# this command changes the ipv4 dns for Ethernet to cloudflare dns you can customize it for your own dns servers 
+Set-DnsClientServerAddress -InterfaceAlias "Ethernet" -ServerAddresses ("1.1.1.1","1.0.0.1")
 
-foreach ($adapter in $ethernetAdapters) {
-    # Check if the adapter is connected
-    if ($adapter.Status -eq "Up") {
-        # Configure DNS settings
-        $adapter | Set-DnsClientServerAddress -ServerAddresses $dnsServers
-        Write-Host "DNS settings updated for Ethernet adapter $($adapter.Name)"
-    } else {
-        Write-Host "Ethernet adapter $($adapter.Name) is not connected, skipping..."
-    }
-}
+# this command changes the ipv4 dns for wifi to the cloudflare dns you can customize it for your own dns
+Set-DnsClientServerAddress -InterfaceAlias "Wi-fi" -ServerAddresses ("1.1.1.1","1.0.0.1")
 
-# Print out the current DNS after switch
-$ipconfigOutput = ipconfig /all
-
-$dnsLines = $ipconfigOutput | Select-String -Pattern "DNS Servers"
-
-$ipv4DnsLines = $dnsLines | Where-Object { $_.Line -match "IPv4 Address" }
-
-if ($ipv4DnsLines) {
-    Write-Host "IPv4 DNS Servers for Ethernet:"
-    foreach ($line in $ipv4DnsLines) {
-        $dnsServer = $line.Line -replace ".*: ", ""
-        Write-Host "  $dnsServer"
-    }
-} else {
-    Write-Host "No IPv4 DNS servers found for Ethernet most likely because computer is using ipv6 DNS servers"
-}
